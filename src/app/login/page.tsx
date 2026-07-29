183b2918906ed47728dd7dffcbe0f506af8f080e
Muraiana Olamide Sherif <olly2ilevel@gmail.com>
2026-07-29 04:26:14 +0100

    org comment demo account text from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,8 +64,8 @@ export default function LoginPage() {
           </button>
         </form>
         <p className="mt-4 rounded-xl bg-mist p-3 text-xs text-slate-500">
-  {/* Demo account: <b>admin@digitalangel.com</b> / <b>angel1234</b> */}
-</p>
+          {/*Demo account: <b>admin@digitalangel.com</b> / <b>angel1234</b>*/}
+        </p>
       </div>
     </div>
   );
